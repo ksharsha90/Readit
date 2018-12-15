44f6df890786cc206960f9c5d40812796b630fb3
lallhar <lfc5timesktt@gmail.com>
2018-12-15 15:17:44 +0530

    Used helper methods

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,7 +1,26 @@
 class StoriesController < ApplicationController
-  def index
-  	@current_time = Time.now
-  	
+  def index  	
   	@story = Story.order('RANDOM()').first
   end
+
+  def new
+  	@story = Story.new
+  end
+
+  def create
+  	@story = Story.new(story_params)
+  	if @story.save
+  		flash[:notice] = "Story submission succeeded"
+  		redirect_to stories_path
+  	else
+  		render action: 'new'
+  	end
+  end
+
+  private
+
+  def story_params
+  	params.require(:story).permit(:name, :link)
+  end
+
 end
