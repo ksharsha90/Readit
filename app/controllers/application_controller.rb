f4a5e36f1b290db9b459a37f9f76233ae0262ec4
lallhar <lfc5timesktt@gmail.com>
2018-12-16 14:02:59 +0530

    Added user login session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+	before_action :current_user
+
+	private
+
+	def current_user
+		return unless session[:user_id]
+		@current_user = User.where(id: session[:user_id]).first
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  resource :session
   get 'votes/create'
   resources :stories do
   	resources :votes
