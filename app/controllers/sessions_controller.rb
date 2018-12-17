380398be179a0f5283d91b9d542a7224999b3776
lallhar <lfc5timesktt@gmail.com>
2018-12-17 21:56:53 +0530

    other commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	@current_user = User.find_by(email: params[:email])
-  	if @current_user && @current_user.authenticate(params[:password])
-  		session[:user_id] = @current_user.id
-  		redirect_to stories_path
-  	else
-  		render action: 'new'
-  	end
-  end
+session[:user_id] = @current_user.id
+if session[:return_to]
+redirect_to session[:return_to]
+session[:return_to] = nil
+else
+redirect_to stories_path
+end
+end
 
   def destroy
   	session[:user_id] = @current_user = nil
