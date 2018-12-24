4a5871765dd6508f0b3d1bc061d7cc599715cb04
ksharsha90 <ksharsha90@gmail.com>
2018-12-24 12:26:01 +0530

    Modifications

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @current_user = User.find_by(email: params[:email])
+if @current_user && @current_user.authenticate(params[:password])
 session[:user_id] = @current_user.id
-if session[:return_to]
-redirect_to session[:return_to]
-session[:return_to] = nil
-else
 redirect_to stories_path
+else
+render action: 'new'
 end
 end
 
